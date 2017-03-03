f156905388fa0b71e8a8c5be02710b366fa337f3
심철환 <a@akaiv.com>
2017-03-03 22:22:54 +0800

    Add list in board

--- src/containers/BoardRead.js
+++ src/containers/BoardRead.js
@@ -1,9 +1,11 @@
+import _ from 'lodash'
 import React from 'react'
 
 class BoardRead extends React.Component {
+  // TODO: Remove repeated videoStorage
   constructor(props) {
     super(props)
-    this.state = { newListName: '' }
+    this.state = { newListName: '', videoStorage: {} }
     this.onPressEnter = this.onPressEnter.bind(this)
   }
 
@@ -11,7 +13,10 @@ class BoardRead extends React.Component {
     const localVideoStorage = localStorage.videoStorage
 
     if (localVideoStorage) {
-      this.setState({ videoStorage: JSON.parse(localVideoStorage) })
+      this.setState({
+        newListName: this.state.newListName,
+        videoStorage: JSON.parse(localVideoStorage)
+      })
     }
   }
 
@@ -24,16 +29,41 @@ class BoardRead extends React.Component {
   }
 
   onPressEnter() {
+    // TODO: Can not refresh parent component
+    // TODO: setState syntax too complicated
     const name = this.state.newListName
+    const currentVideoStorage = this.state.videoStorage
+    const currentBoardName = this.props.params.boardName
+    const currentBoard = _.find(currentVideoStorage.boards, o => {return o.name === currentBoardName})
 
     if (name) {
-      console.log(name)
+      if (currentBoard.lists.indexOf(name) === -1) {
+        this.setState({
+          newListName: '',
+          videoStorage: {
+            boards: currentVideoStorage.boards.map(board => {
+              if (board === currentBoard) {
+                return { name: board.name, lists: [ ...board.lists, name ] }
+              } else {
+                return board
+              }
+            }),
+            videos: currentVideoStorage.videos
+          }
+        })
+      } else {
+        console.log('FAIL: List exists')
+      }
     } else {
       console.log('List name is required')
     }
   }
 
   render() {
+    const currentVideoStorage = this.state.videoStorage
+    const currentBoardName = this.props.params.boardName
+    const currentBoard = _.find(currentVideoStorage.boards, o => {return o.name === currentBoardName})
+
     return (
       <section>
         <h1>{this.props.params.boardName}</h1>
@@ -42,6 +72,8 @@ class BoardRead extends React.Component {
           onChange={event => this.setState({ newListName: event.target.value })}
           onKeyPress={event => {if (event.key === 'Enter') this.onPressEnter()}}
           value={this.state.newListName} />
+
+        <ul>{currentBoard.lists.map(list => {return (<li key={list}>{list}</li>)})}</ul>
       </section>
     )
   }
