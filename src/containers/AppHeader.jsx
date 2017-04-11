b158a73dde5c3571d3da8b977431874b5a9ea2bf
심철환 <a@akaiv.com>
2017-04-11 20:02:00 +0200

    Add signing links to AppHeader

--- src/containers/AppHeader.jsx
+++ src/containers/AppHeader.jsx
@@ -8,6 +8,17 @@ const AppHeader = () => {
       <h1 className="AppTitle">
         <Link to="/">Video Archives <small>alpha</small></Link>
       </h1>
+
+      {true ?
+        <nav className="AppNav">
+          <Link to="/signup">Sign up</Link>
+          <Link to="/signin">Sign in</Link>
+        </nav>
+      :
+        <nav className="AppNav">
+          <Link to="/signout">Sign out</Link>
+        </nav>
+      }
     </header>
   )
 }
