bb9c2af40335c772bb18dcbefa2b83551103c447
심철환 <a@akaiv.com>
2017-04-18 20:39:48 +0900

    Add sample config file

---
+++ src/config.sample.js
@@ -0,0 +1,10 @@
+export const youtubeAPIKey = ''
+
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+}
