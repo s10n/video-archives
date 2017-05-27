a7232d8a37f6522bb6cef5c3b7fdb85c19d67a24
심철환 <a@akaiv.com>
2017-05-28 00:17:14 +0900

    Split routes as a file

--- src/containers/AppMain.jsx
+++ src/containers/AppMain.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import routes from '../routes'
 import './AppMain.css'
-import PageFront from './PageFront'
-import PageTrash from './PageTrash'
-import BoardRead from './BoardRead'
-import PageSignup from './auth/PageSignup'
-import PageSignin from './auth/PageSignin'
-import PageSignout from './auth/PageSignout'
 
 const AppMain = () => {
   return (
     <main className="AppMain">
       <div className="PageWrapper">
-        <Switch>
-          <Route exact path="/" component={PageFront} />
-          <Route exact path="/trash" component={PageTrash} />
-          <Route exact path="/signup" component={PageSignup} />
-          <Route exact path="/signin" component={PageSignin} />
-          <Route exact path="/signout" component={PageSignout} />
-          <Route path="/:boardSlug" component={BoardRead} />
-        </Switch>
+        {routes}
       </div>
     </main>
   )
