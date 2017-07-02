5aabb156bbd391fa28b4e7b08a268cd3037fb48f
심철환 <a@akaiv.com>
2017-07-02 20:07:30 +0900

    Rename reducers file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 import { reducer as form } from 'redux-form'
-import auth from './ReducerAuth'
-import boards from './ReducerBoards'
-import videos from './ReducerVideos'
+import auth from './authReducer'
+import boards from './boardsReducer'
+import videos from './videosReducer'
 
 const rootReducer = combineReducers({ auth, boards, videos, router, form })
 
