2035f3ee9db4d74b921604f65c809b61471d1ce8
심철환 <a@akaiv.com>
2017-07-02 21:29:00 +0900

    Add app reducer

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,8 @@
+import * as types from '../actions/types'
+
+export default function(state = {}, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 import { reducer as form } from 'redux-form'
+import app from './appReducer'
 import auth from './authReducer'
 import boards from './boardsReducer'
 import videos from './videosReducer'
 
-const rootReducer = combineReducers({ auth, boards, videos, router, form })
+const rootReducer = combineReducers({ app, auth, boards, videos, router, form })
 
 export default rootReducer
