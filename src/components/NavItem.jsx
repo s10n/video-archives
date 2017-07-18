ef66e87b6090e8273b3b27a70954c922a906a528
심철환 <a@akaiv.com>
2017-07-18 23:22:51 +0900

    Implement drag and drop: List >> Trash

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -9,10 +9,17 @@ const propTypes = {
   count: PropTypes.number,
   trash: PropTypes.bool,
   connectDropTarget: PropTypes.func.isRequired,
-  isOver: PropTypes.bool.isRequired
+  isOver: PropTypes.bool.isRequired,
+  canDrop: PropTypes.bool.isRequired
 }
 
 const boardTarget = {
+  canDrop(props, monitor) {
+    const { board } = props
+    const { list } = monitor.getItem()
+    return !(list && board)
+  },
+
   drop(props) {
     return !props.trash ? { board: props.board } : { trash: true }
   }
@@ -21,13 +28,14 @@ const boardTarget = {
 const collect = (connect, monitor) => {
   return {
     connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
   }
 }
 
-const NavItem = ({ board, count, trash, connectDropTarget, isOver }) => {
+const NavItem = ({ board, count, trash, connectDropTarget, isOver, canDrop }) => {
   return connectDropTarget(
-    <div className={isOver ? 'NavItem canDrop' : 'NavItem'}>
+    <div className={(isOver && canDrop) ? 'NavItem canDrop' : 'NavItem'}>
       {!trash ? <span>{board.title}</span> : <span>Trash</span>}
       <span className="count">{count}</span>
     </div>
