550b1c437d870f2f5385c1743adee0b8e10cda36
심철환 <a@akaiv.com>
2017-08-07 23:32:40 +0900

    Fix error

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,10 +3,10 @@ import types from '../constants/types'
 export default function(state = {}, action) {
   switch (action.type) {
     case types.AUTH_USER:
-      return { ...state, authenticated: true, user: action.user, error: {} }
+      return { ...state, authenticated: true, user: action.user, error: '' }
 
     case types.UNAUTH_USER:
-      return { ...state, authenticated: false, user: {}, error: {} }
+      return { ...state, authenticated: false, user: {}, error: '' }
 
     case types.AUTH_ERROR:
       return { ...state, error: action.error }
