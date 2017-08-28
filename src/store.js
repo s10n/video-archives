08dd9a15fdf9d5ee221b8b035f3c16337bb8e3af
심철환 <a@akaiv.com>
2017-08-28 23:47:15 +0900

    Rename import name of redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import reduxThunk from 'redux-thunk'
+import thunk from 'redux-thunk'
 import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
 import reducer from './reducers'
@@ -10,5 +10,5 @@ export const history = createHistory()
 
 export default createStore(
   reducer,
-  composeEnhancers(applyMiddleware(reduxThunk, routerMiddleware(history)))
+  composeEnhancers(applyMiddleware(thunk, routerMiddleware(history)))
 )
