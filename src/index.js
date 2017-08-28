70e60e4399b7721fc57b45f4f3862f79725490ce
심철환 <a@akaiv.com>
2017-08-28 23:46:33 +0900

    Export auth() instead of auth from api.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './containers/App'
 process.env.REACT_APP_ENV === 'production' &&
   Raven.config('https://6e6f68aaf3a14526aa3880cf5353b7b1@sentry.io/200634').install()
 
-auth().onAuthStateChanged(user => {
+auth.onAuthStateChanged(user => {
   user
     ? store.dispatch({ type: types.AUTH_USER, user })
     : store.dispatch({ type: types.UNAUTH_USER })
